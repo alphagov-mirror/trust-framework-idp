9ea61c62c3e5a42dd2420c13eddd898d64d75c9e
brenetic <b@brenetic.com>
2019-12-12 17:18:14 +0000

    Correctly configure for deploying 2 IDP apps to PaaS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,5 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.broker_domain = IDP['broker_domain']
+  config.broker_domain = ENV.fetch('BROKER_DOMAIN')
 end
